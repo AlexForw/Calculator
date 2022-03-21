2adba9c8dcbf653547b36961c6876536fe4fe60e
AlexForw <74962183+AlexForw@users.noreply.github.com>
2022-03-21 19:37:28 +0300

    feat: add substraction and half an multiply func

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -3,26 +3,28 @@ const inputNum = document.querySelector('.input-num')
 const inputExpression = document.querySelector('.input-expression')
 let count = ''
 let sum = 0
-
+let sign = []
 
 buttonClick.addEventListener('click',function(elem){
     let currentCell = elem.target
-    
+
     if(currentCell.classList.contains('input'))return
-    
+
     if(!(currentCell.classList.contains('item')))return
-    
+
     if(currentCell.classList.contains('back')){
         deleteMainValue()
         return
     }
-    
+
     if(currentCell.classList.contains('clean')){
         deleteAllValues()
         return
     }
-    
+
     if(currentCell.classList.contains('sum')){
+        equal()
+        sign.push('+')
         sum += +count
         inputExpression.textContent = sum + ' +'
         inputNum.textContent = 0
@@ -30,11 +32,66 @@ buttonClick.addEventListener('click',function(elem){
         return
     }
 
-  
+    if(currentCell.classList.contains('sub')){
+
+
+        if(sum == 0){
+            equal()
+            sign.push('-')
+            sum = +count
+            inputExpression.textContent = sum + ' -'
+            inputNum.textContent = 0
+            count = ''
+            return
+        }
+        equal()
+        sign.push('-')
+        sum -= +count
+        inputExpression.textContent = sum + ' -'
+        inputNum.textContent = 0
+        count = ''
+        return
+    }
+
+    if(currentCell.classList.contains('equal')){
+        equal()
+        inputExpression.textContent = ''
+        inputNum.textContent = sum
+        sign = []
+        sign.push('=')
+        count = ''
+        return
+    }
+
+    if(currentCell.classList.contains('multiply')){
+        if(sum == 0){
+            equal()
+            sign.push('*')
+            sum = +count
+            inputExpression.textContent = sum + ' x'
+            inputNum.textContent = 0
+            count = ''
+            return
+        }
+        // equal()
+        sign.push('*')
+        console.log(sum);
+        sum *= +inputNum.textContent
+        console.log(sum);
+        inputExpression.textContent = sum + ' x'
+        inputNum.textContent = 0
+        count = ''
+        return
+    }
+
 
     addNum(currentCell)
-    
+
 })
+
+
+
+
 
 function deleteMainValue() {
     count = ''
@@ -42,6 +99,7 @@ function deleteMainValue() {
 }
 
 function deleteAllValues() {
+    sign = []
     sum = 0
     count = ''
     inputExpression.textContent = ''
@@ -49,6 +107,25 @@ function deleteAllValues() {
 }
 
 function addNum(elem) {
+    if(sign.slice(-1) == '='){
+        sum = 0
+    }
     count += elem.textContent
     inputNum.textContent = count
+
+}
+
+function equal(){
+    if(sign.slice(-1) == '+') {
+        sum += +count
+        count = ''
+    }
+    if(sign.slice(-1) == '-') {
+        sum -= +count
+        count = ''
+    }
+    if(sign.slice(-1) == '*'){
+        sum *= +count
+        count = ''
+    }
 }
