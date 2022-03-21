09325d417ac91d0f397f6e079454eee5baf8b5e0
AlexForw <74962183+AlexForw@users.noreply.github.com>
2022-03-21 23:16:11 +0300

    feat: start adding divide func and work with mult

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,3 +1,136 @@
+// const buttonClick = document.querySelector('.container')
+// const inputNum = document.querySelector('.input-num')
+// const inputExpression = document.querySelector('.input-expression')
+// let count = ''
+// let sum = 0
+// let sign = []
+// let wordCount = ''
+
+// buttonClick.addEventListener('click',function(elem){
+//     let currentCell = elem.target
+
+//     if(currentCell.classList.contains('input'))return
+
+//     if(!(currentCell.classList.contains('item')))return
+
+//     if(currentCell.classList.contains('back')){
+//         deleteMainValue()
+//         return
+//     }
+
+//     if(currentCell.classList.contains('clean')){
+//         deleteAllValues()
+//         return
+//     }
+
+//     if(currentCell.classList.contains('sum')){
+//         changeSign()
+//         sign.push('+')
+//         sum += +count
+//         inputExpression.textContent = sum + ' +'
+//         inputNum.textContent = 0
+//         count = ''
+//         return
+//     }
+
+//     if(currentCell.classList.contains('sub')){
+
+
+//         if(sum == 0){
+//             changeSign()
+//             sign.push('-')
+//             sum = +count
+//             inputExpression.textContent = sum + ' -'
+//             inputNum.textContent = 0
+//             count = ''
+//             return
+//         }
+//         changeSign()
+//         sign.push('-')
+//         sum -= +count
+//         inputExpression.textContent = sum + ' -'
+//         inputNum.textContent = 0
+//         count = ''
+//         return
+//     }
+
+//     if(currentCell.classList.contains('equal')){
+//         changeSign()
+//         inputExpression.textContent = ''
+//         inputNum.textContent = sum
+//         sign = []
+//         sign.push('=')
+//         count = ''
+//         return
+//     }
+
+//     if(currentCell.classList.contains('multiply')){
+//         if(sum == 0){
+//             changeSign()
+//             sign.push('*')
+//             sum = +count
+//             inputExpression.textContent = sum + ' x'
+//             inputNum.textContent = 0
+//             count = ''
+//             return
+//         }
+//         changeSign()
+//         sign.push('*')
+//         console.log(sum);
+//         sum *= +inputNum.textContent
+//         console.log(sum);
+//         inputExpression.textContent = sum + ' x'
+//         inputNum.textContent = 0
+//         count = ''
+//         return
+//     }
+    
+//     addNum(currentCell)
+    
+//   })
+  
+  
+  
+  
+  
+//   function deleteMainValue() {
+//     count = ''
+//     inputNum.textContent = 0
+// }
+
+// function deleteAllValues() {
+//   sign = []
+//   sum = 0
+//   count = ''
+//   inputExpression.textContent = ''
+//   inputNum.textContent = 0
+// }
+
+// function addNum(elem) {
+//   if(sign.slice(-1) == '='){
+//     sum = 0
+//   }
+//   wordCount += elem.textContent
+//   if(wordCount.slice(-1) == 0 && wordCount.slice(-1) != '')return
+//   count += elem.textContent
+//   inputNum.textContent = count
+// }
+
+// function changeSign(){
+//   if(sign.slice(-1) == '+') {
+//         sum += +count
+//         count = ''
+//     }
+//     if(sign.slice(-1) == '-') {
+//         sum -= +count
+//         count = ''
+//     }
+//     if(sign.slice(-1) == '*'){
+//         sum *= +count
+//         count = ''
+//     }
+// }
+
 const buttonClick = document.querySelector('.container')
 const inputNum = document.querySelector('.input-num')
 const inputExpression = document.querySelector('.input-expression')
@@ -73,7 +206,7 @@ buttonClick.addEventListener('click',function(elem){
             count = ''
             return
         }
-        // equal()
+        equal()
         sign.push('*')
         console.log(sum);
         sum *= +inputNum.textContent
@@ -83,6 +216,27 @@ buttonClick.addEventListener('click',function(elem){
         count = ''
         return
     }
+
+    if(currentCell.classList.contains('divide')) {
+        if (sum == 0) {
+            equal()
+            sign.push('/')
+            sum = +count
+            inputExpression.textContent = sum + ' /'
+            inputNum.textContent = 0
+            count = ''
+            return
+        }
+        sign.push('/')
+        console.log(sum);
+        sum /= +inputNum.textContent
+        console.log(sum);
+        inputExpression.textContent = sum + ' /'
+        inputNum.textContent = 0
+        count = ''
+        return
+    }
+    
 
 
     addNum(currentCell)
@@ -126,6 +280,10 @@ function equal(){
     }
     if(sign.slice(-1) == '*'){
         sum *= +count
+        count = ''
+    }
+    if(sign.slice(-1) == '/'){
+        sum /= +count
         count = ''
     }
 }
